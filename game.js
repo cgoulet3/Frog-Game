c979f314b087d0223df85f20ab7fa8c1adb6996d
cgoulet3 <cgoulet3@gatech.edu>
2020-11-22 23:43:16 -0500

    Adding HTML, CSS, and Javascript files

---
+++ game.js
@@ -0,0 +1,101 @@
+// Initialize game variables
+let isGameOver = false;
+let scoreCount = 0;
+const game = document.querySelector(".game");
+const frog = document.createElement("div");
+const alligator = document.createElement("div");
+const score = document.querySelector(".score");
+
+// Function to run the game
+function runGame() {	
+	frog.classList.add("frog");
+	game.appendChild(frog);
+	
+	alligator.classList.add("alligator");
+	game.appendChild(alligator);
+	
+	document.addEventListener("keydown", function(event) {
+		if (event.keyCode == 32) {
+			jump();
+		}
+	});
+	
+	//generateAlligators();
+	
+	let isAlive = setInterval(function() {
+		let frogTop = parseInt(window.getComputedStyle(frog).getPropertyValue("top"));
+		let alligatorLeft = parseInt(window.getComputedStyle(alligator).getPropertyValue("left"));
+		
+		// Detect collision
+		if (alligatorLeft > 0 && alligatorLeft < 50) {
+			if (frogTop >= 100) {
+				clearInterval(isAlive);
+				gameOver();
+			} else {
+				scoreCount += 10;
+				score.innerHTML = "Score: " + scoreCount;
+			}
+		}		
+		
+		if (alligatorLeft < 0) {
+			document.getElementsByClassName("alligator")[0].style.backgroundColor = "transparent";
+		} else if (alligatorLeft > 550) {
+			document.getElementsByClassName("alligator")[0].style.backgroundColor = "black";
+		}
+		
+		
+	}, 10);
+	
+}
+
+// Function to make the frog jump
+function jump() {
+	if (!frog.classList.contains("jump")) {
+		frog.classList.add("jump");
+		setTimeout(function() {
+			frog.classList.remove("jump");
+		}, 500)
+	}
+}
+
+// Function to generate alligators
+//function generateAlligators() {
+//	let random = Math.random * 4000;
+//	const alligator = document.createElement("div");
+//	alligator.classList.add("alligator");
+//	game.appendChild(alligator);
+//	
+//	let timerId = setInterval(function() {
+//		let frogTop = parseInt(window.getComputedStyle(frog).getPropertyValue("top"));
+//		let alligatorLeft = parseInt(window.getComputedStyle(alligator).getPropertyValue("left"));
+//		console.log(alligatorLeft);
+//		
+//		// Detect collision
+//		if (alligatorLeft > 0 && alligatorLeft < 50) {
+//			if (frogTop >= 100) {
+//				clearInterval(isAlive);
+//				gameOver();
+//			} else {
+//				scoreCount += 10;
+//				score.innerHTML = "Score: " + scoreCount;
+//			}
+//		}		
+//		
+//		if (alligatorLeft < 0) {
+//			document.getElementsByClassName("alligator")[0].style.backgroundColor = "transparent";
+//		} else if (alligatorLeft > 550) {
+//			document.getElementsByClassName("alligator")[0].style.backgroundColor = "black";
+//		}	
+//		
+//		alligator.style.left -= 10 + "px";
+//	}, 10)
+//	
+//	//setTimeout(generateAlligators, random);
+//}
+
+// Function to reset the game
+function gameOver() {
+	alert("Game Over!");
+}
+
+runGame();
